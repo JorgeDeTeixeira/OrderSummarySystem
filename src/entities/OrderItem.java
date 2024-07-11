6934d8b1c0206ee8848ea0f3e2d61c3bef235428
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-07-11 10:48:10 -0300

    atualização da classe OrderItem

--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -4,12 +4,15 @@ public class OrderItem {
 	private Integer quantity;
 	private Double price;
 
+	private Product product;
+
 	public OrderItem() {
 	}
 
-	public OrderItem(Integer quantity, Double price) {
+	public OrderItem(Integer quantity, Double price, Product product) {
 		this.quantity = quantity;
 		this.price = price;
+		this.product = product;
 	}
 
 	public Integer getQuantity() {
@@ -28,8 +31,22 @@ public class OrderItem {
 		this.price = price;
 	}
 
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
 	public double subTotal() {
 		return quantity * price;
+	}
+
+	@Override
+	public String toString() {
+		return product.getName() + ", $" + String.format("%.2f", price) + ", Quantity: " + quantity + ", Subtotal: $"
+				+ String.format("%.2f", subTotal());
 	}
 
 }
